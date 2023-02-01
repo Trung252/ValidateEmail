bae2e0228e0f9904333eb40081619cf6ff267ece
Trung252 <pkt02052000@gmail.com>
2023-02-01 18:36:49 +0700

    first commit

---
+++ ValidateEmail/validateEmail.js
@@ -0,0 +1,7 @@
+function validate_email(str) {
+    regexp = /^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)$/;
+    if (regexp.test(str)) {
+        return true;
+    } 
+    return false;
+}
